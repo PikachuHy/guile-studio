58c2bc354f51c4efe0de04f24b7a2398a83eafde
Ricardo Wurmus <rekado@elephly.net>
2019-12-16 07:59:20 +0100

    Bump version to 0.0.2.

--- guix/rekado/guile-studio.scm
+++ guix/rekado/guile-studio.scm
@@ -14,7 +14,7 @@
 (define-public guile-studio
   (package
     (name "guile-studio-devel")
-    (version "0.0.1")
+    (version "0.0.2")
     (source (local-file (string-append "../../guile-studio-" version ".tar.gz")))
     (build-system gnu-build-system)
     (arguments
